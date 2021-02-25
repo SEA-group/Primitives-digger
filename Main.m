752d7c64d819645dec8d81fa255efa5f2ce3a7bc
AstreTunes <mebius.uranus@gmail.com>
2021-02-25 09:52:53 +0100

    Modify output path

--- Main.m
+++ Main.m
@@ -1,5 +1,5 @@
 % This script can help adding a pair of vertex & index chunks (i.e. one renderSet) into a bunch of .primitives files
-% ver.2020.12.12
+% ver.2021.02.25
 % requirement:
 % - PrimitivesAppend_Mk1.m
 % - PrimitivesAppend32_Mk1.m
@@ -17,59 +17,62 @@ close all;
 
 fileNames = dir('Queue/*.primitives');
 
-chunkList{1}.name = 'Yamato_Propulsor';
+chunkList{1}.name = 'Test';
 chunkList{1}.wire = 0;
-chunkList{1}.list32 = 0;
-
-chunkList{2}.name = 'Yamato_TorpedoTube';
-chunkList{2}.wire = 0;
-chunkList{2}.list32 = 0;
-
-chunkList{3}.name = 'Yamato_Lens_Blue';
-chunkList{3}.wire = 0;
-chunkList{3}.list32 = 1;
-
-chunkList{4}.name = 'Yamato_Lens_Blue_alpha';
-chunkList{4}.wire = 0;
-chunkList{4}.list32 = 0;
-
-chunkList{5}.name = 'Yamato_Lens_Red';
-chunkList{5}.wire = 0;
-chunkList{5}.list32 = 0;
-
-chunkList{6}.name = 'Yamato_Lens_Red_alpha';
-chunkList{6}.wire = 0;
-chunkList{6}.list32 = 0;
-
-chunkList{7}.name = 'Yamato_Mechanical1_A';
-chunkList{7}.wire = 0;
-chunkList{7}.list32 = 1;
-
-chunkList{8}.name = 'Yamato_Mechanical1_B';
-chunkList{8}.wire = 0;
-chunkList{8}.list32 = 1;
-
-chunkList{9}.name = 'Yamato_Mechanical1_C';
-chunkList{9}.wire = 0;
-chunkList{9}.list32 = 1;
-
-chunkList{10}.name = 'Yamato_Mechanical1_D';
-chunkList{10}.wire = 0;
-chunkList{10}.list32 = 1;
+chunkList{1}.list32 = 1;
+% 
+% chunkList{2}.name = 'Test';
+% chunkList{2}.wire = 1;
+% chunkList{2}.list32 = 0;
+% 
+% chunkList{3}.name = 'Yamato_Lens_Blue';
+% chunkList{3}.wire = 0;
+% chunkList{3}.list32 = 0;
+% 
+% chunkList{4}.name = 'Yamato_Lens_Blue_alpha';
+% chunkList{4}.wire = 0;
+% chunkList{4}.list32 = 0;
+% 
+% chunkList{5}.name = 'Yamato_Lens_Red';
+% chunkList{5}.wire = 0;
+% chunkList{5}.list32 = 0;
+% 
+% chunkList{6}.name = 'Yamato_Lens_Red_alpha';
+% chunkList{6}.wire = 0;
+% chunkList{6}.list32 = 0;
+% 
+% chunkList{7}.name = 'Yamato_Mechanical1_A';
+% chunkList{7}.wire = 0;
+% chunkList{7}.list32 = 1;
+% 
+% chunkList{8}.name = 'Yamato_Mechanical1_B';
+% chunkList{8}.wire = 0;
+% chunkList{8}.list32 = 1;
+% 
+% chunkList{9}.name = 'Yamato_Mechanical1_C';
+% chunkList{9}.wire = 0;
+% chunkList{9}.list32 = 1;
+% 
+% chunkList{10}.name = 'Yamato_Mechanical1_D';
+% chunkList{10}.wire = 0;
+% chunkList{10}.list32 = 1;
 
 % !!!Attention!!! 
 % "wire" type vertex chunk cannot be added to skinned model 
 
 %% Processing
 
+if ~exist('./Output', 'dir') 
+    mkdir('./Output') 
+end
+
 for indFileInFolder = 1: size(fileNames, 1)
     
-    fileName = ['Queue/', fileNames(indFileInFolder).name];
+    inputFileName = ['Queue/', fileNames(indFileInFolder).name];
+    outputFileName = ['Output/', fileNames(indFileInFolder).name];
     
     % Create backup and load primitives content
-    backupFileName = [fileName, 'bak'];
-    movefile(fileName, backupFileName);
-    originalFile=fopen(backupFileName, 'r');
+    originalFile=fopen(inputFileName, 'r');
     primCode=fread(originalFile);
     fclose(originalFile);
     clear originalFile;
@@ -95,7 +98,7 @@ for indFileInFolder = 1: size(fileNames, 1)
     end
 
     % Write to file
-    newFile=fopen(fileName, 'w');
+    newFile=fopen(outputFileName, 'w');
     fwrite(newFile, primCode);
     fclose(newFile);
     
--- README.md
+++ README.md
@@ -9,10 +9,6 @@ These scripts can add a pair of vertex and indice sections into a bunch of *.pri
 
 ## How to use
 1. Put your *.primitives* files into `Queue/`
-2. Open `Main.m`, assign the new section name in line 20; toggle "wire" vertex type in line 21; toggle "list32" in line 22; "skinned" property will be detected by the script automaticaly. You can add several sections at once by adding multple items in *chunkList{}*.
+2. Open `Main.m`. In "%% Parameters" part, assign the new section name, toggle "wire" vertex type and toggle "list32" triangle list type. The "skinned" property will be detected by the script automaticaly. You can add several sections at once by adding multple items in *chunkList{}*.
 3. Run `Main.m`
-4. Now the sections are added, Original *.primitives* files are backed up and renamed to **.primitivesbak*.
-
-## Attention
-1. **The backup will be overwritten if you run the script twice**
-2. This tool helps you to dig a hole （only one) on the file, but you still need a dedicated model tool to put your 3D mesh inside.
+4. New *.primitives* files will appear in  `Output/`, original *.primitives* files will stay intact in `Queue/`.
